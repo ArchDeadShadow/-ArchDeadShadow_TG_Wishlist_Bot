2b01bbea86d96446ef66a3cce4c476b427f8d192
Sergey Rudenko <sergeyrudenkosr@gmail.com>
2025-08-29 11:33:00 +0300

    Correction of small errors. Iliteration by array, verification whether the gift was bought

--- bot.js
+++ bot.js
@@ -7,12 +7,17 @@ const BOT_TOKEN = process.env.BOT_TOKEN
 
 const ADMIN_ID = Number(process.env.ADMIN_ID)
 
-const wishlist = JSON.parse(fs.readFileSync('./src/wishlist.json', 'utf8' ))
+const wishlist = JSON.parse(fs.readFileSync('wishlist.json', 'utf8'))
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Этот список желаний содержит:'))
 bot.command('wishlist', (ctx) => {
-    ctx.reply('Тут весь список желаний')
+    const gifts = wishlist.map((gift, index) => {
+        const bought = gift.boughtBy ? `(куплено${gift.boughtBy})` : `(не куплено)`
+        const row = `${index + 1}. ${gift.title} ${bought}`
+        return row
+    })
+    ctx.reply(`Список подарков:\n${gifts.join('\n')}`)
 })
 bot.command('add', (ctx) => {
     ctx.reply('Добавить желаемое')
